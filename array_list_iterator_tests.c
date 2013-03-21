9ed3efaa2ca13aa09653627779da6bca256f836a
Mark Withall <github@freudiantypo.com>
2013-03-21 21:38:21 +0000

    Added more iterator tests.

--- array_list_iterator_tests.c
+++ array_list_iterator_tests.c
@@ -31,7 +31,40 @@ TEST(first_move_next_moves_to_first_item)
     l->move_next(l);
     i = l->get_current(l).as_int;
     if (i != 10)
-        return "current value expected 10";
+        return "Current value expected 10";
+END_TEST
+
+TEST(first_move_next_returns_true)
+    if (l->move_next(l) == 0)
+        return "Expected true but was false";
+END_TEST
+
+TEST(second_move_next_returns_false)
+    l->move_next(l);
+    if (l->move_next(l) != 0)
+        return "Expected false but was true";
+END_TEST
+
+TEST(second_move_next_resets)
+    int i;
+
+    l->move_next(l); /* moves to 10 */
+    l->move_next(l); /* moves to end and resets */
+    l->move_next(l); /* moves to 10 */
+    i = l->get_current(l).as_int;
+    if (i != 10)
+        return "Current value expected 10";
+END_TEST
+
+TEST(reset_returns_to_beginning)
+    int i;
+
+    l->move_next(l); /* moves to 10 */
+    l->reset(l);
+    l->move_next(l); /* moves to 10 */
+    i = l->get_current(l).as_int;
+    if (i != 10)
+        return "Current value expected 10";
 END_TEST
 
 int main(void)
@@ -39,6 +72,10 @@ int main(void)
     test_fixture("array_list iterator tests");
 
     run_test(first_move_next_moves_to_first_item);
+    run_test(first_move_next_returns_true);
+    run_test(second_move_next_returns_false);
+    run_test(second_move_next_resets);
+    run_test(reset_returns_to_beginning);
 
     display_results();
 
