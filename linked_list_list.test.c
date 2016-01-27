071b675f641953776ff0175fdd2fafe2df5781ba
Mark Withall <github@markwithall.com>
2016-01-27 14:18:40 +0000

    Make tests use TESTickle.

--- linked_list_list.test.c
+++ linked_list_list.test.c
@@ -1,4 +1,4 @@
-#include "test.h"
+#include "TESTickle.h"
 
 #include "linked_list.h"
 
@@ -27,67 +27,67 @@ teardown()
 
 TEST(get_element_at_returns_expected)
     int item = l->get_element_at(l, 1).as_int;
-    ASSERT_EQUAL(item, 20, "Actual %d, expected %d");
+    ASSERT_EQUAL(item, 20, "%d", "");
 END_TEST
 
 TEST(set_element_at_sets_element)
     int item;
     l->set_element_at(l, 1, data_for(15));
     item = l->get_element_at(l, 1).as_int;
-    ASSERT_EQUAL(item, 15, "Actual %d, expected %d");
+    ASSERT_EQUAL(item, 15, "%d", "");
 END_TEST
 
 TEST(set_element_does_not_change_count)
     int count;
     l->set_element_at(l, 1, data_for(15));
     count = l->get_count(l);
-    ASSERT_EQUAL(count, 3, "Actual %d, expected %d");
+    ASSERT_EQUAL(count, 3, "%d", "");
 END_TEST
 
 TEST(index_of_returns_correct_index)
     int index = l->index_of(l, data_for(20));
-    ASSERT_EQUAL(index, 1, "Actual %d, expected %d");
+    ASSERT_EQUAL(index, 1, "%d", "");
 END_TEST
 
 TEST(insert_at_inserts_item)
     int item;
     l->insert_at(l, 1, data_for(15));
     item = l->get_element_at(l, 1).as_int;
-    ASSERT_EQUAL(item, 15, "Actual %d, expected %d");
+    ASSERT_EQUAL(item, 15, "%d", "");
 END_TEST
 
 TEST(insert_at_moves_previous_item_along)
     int item;
     l->insert_at(l, 1, data_for(15));
     item = l->get_element_at(l, 2).as_int;
-    ASSERT_EQUAL(item, 20, "Actual %d, expected %d");
+    ASSERT_EQUAL(item, 20, "%d", "");
 END_TEST
 
 TEST(insert_at_increase_count)
     int count;
     l->insert_at(l, 1, data_for(15));
     count = l->get_count(l);
-    ASSERT_EQUAL(count, 4, "Actual %d, expected %d");
+    ASSERT_EQUAL(count, 4, "%d", "");
 END_TEST
 
 TEST(remove_at_removes_item)
-    int result;
+    int r;
     l->remove_at(l, 1);
-    result = l->contains(l, data_for(20));
-    ASSERT_FALSE(result, "Expected 0, actual %d");
+    r = l->contains(l, data_for(20));
+    ASSERT_FALSE(r, "Expected 0");
 END_TEST
 
 TEST(remove_at_moved_next_item_back)
     int item;
     l->remove_at(l, 1);
     item = l->get_element_at(l, 1).as_int;
-    ASSERT_EQUAL(item, 30, "Actual %d, expected %d");
+    ASSERT_EQUAL(item, 30, "%d", "");
 END_TEST
 
 TEST(remove_at_decreased_count)
     int count;
     l->remove_at(l, 1);
     count = l->get_count(l);
-    ASSERT_EQUAL(count, 2, "Actual %d, expected %d");
+    ASSERT_EQUAL(count, 2, "%d", "");
 END_TEST
 
